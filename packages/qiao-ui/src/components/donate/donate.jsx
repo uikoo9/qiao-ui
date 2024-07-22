c67c872b125ec550c9eb8ed522631c9168a6b73e
uikoo9 <uikoo9@qq.com>
2024-07-22 19:43:28 +0800

    feat(qiao-ui): donate add electron

--- packages/qiao-ui/src/components/donate/donate.jsx
+++ packages/qiao-ui/src/components/donate/donate.jsx
@@ -13,12 +13,12 @@ import { colorLog } from '../../util/log.js';
 export const Donate = (props) => {
   colorLog('qiao-ui/components/donate: render');
 
-  const img =
-    props.type == 'qq' ? (
-      <img src="https://static-insistime.vincentqiao.com/00_insistime/qq.jpeg" alt="qq" />
-    ) : (
-      <img src="https://static-insistime.vincentqiao.com/00_insistime/donate2.png" alt="donate" />
-    );
+  let img;
+  if (props.type == 'qq') img = <img src="https://static-insistime.vincentqiao.com/00_insistime/qq.jpeg" alt="qq" />;
+  if (props.type == 'donate')
+    img = <img src="https://static-insistime.vincentqiao.com/00_insistime/donate2.png" alt="donate" />;
+  if (props.type == 'electron')
+    img = <img src="https://static.vincentqiao.com/qrcode/wechat-electron.jpg" alt="electron" />;
   return (
     <div className="donate">
       <div className="donate-img">{img}</div>
